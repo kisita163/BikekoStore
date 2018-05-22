276bf97773dfb1d4d2d26f7b7485af3cbf85dfa5
Hugues Kisita <kisita2002@yahoo.fr>
2018-05-22 11:57:16 +0200

    Add function that will center the picture in the window

--- src/fxmltableview/FXMLPicturesController.java
+++ src/fxmltableview/FXMLPicturesController.java
@@ -8,6 +8,8 @@ package fxmltableview;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 /**
  * FXML Controller class
@@ -15,7 +17,8 @@ import javafx.fxml.Initializable;
  * @author HuguesKi
  */
 public class FXMLPicturesController implements Initializable {
-
+    
+    private ImageView imageView;
     /**
      * Initializes the controller class.
      */
@@ -23,5 +26,32 @@ public class FXMLPicturesController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    
+    public void centerImage() {
+
+        Image img = imageView.getImage();
+        if (img != null) {
+            double w = 0;
+            double h = 0;
+
+            double ratioX = imageView.getFitWidth() / img.getWidth();
+            double ratioY = imageView.getFitHeight() / img.getHeight();
+
+            double reducCoeff = 0;
+            if (ratioX >= ratioY) {
+                reducCoeff = ratioY;
+            } else {
+                reducCoeff = ratioX;
+            }
+
+            w = img.getWidth() * reducCoeff;
+            h = img.getHeight() * reducCoeff;
+
+            imageView.setX((imageView.getFitWidth() - w) / 2);
+            imageView.setY((imageView.getFitHeight() - h) / 2);
+
+        }
+    }
     
 }
--- src/fxmltableview/FXMLTableViewController.java
+++ src/fxmltableview/FXMLTableViewController.java
@@ -201,30 +201,4 @@ public class FXMLTableViewController implements Initializable {
             e.printStackTrace();
         }
     }
-
-    public void centerImage() {
-
-        Image img = imageView.getImage();
-        if (img != null) {
-            double w = 0;
-            double h = 0;
-
-            double ratioX = imageView.getFitWidth() / img.getWidth();
-            double ratioY = imageView.getFitHeight() / img.getHeight();
-
-            double reducCoeff = 0;
-            if (ratioX >= ratioY) {
-                reducCoeff = ratioY;
-            } else {
-                reducCoeff = ratioX;
-            }
-
-            w = img.getWidth() * reducCoeff;
-            h = img.getHeight() * reducCoeff;
-
-            imageView.setX((imageView.getFitWidth() - w) / 2);
-            imageView.setY((imageView.getFitHeight() - h) / 2);
-
-        }
-    }
 }
